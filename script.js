1a7327e76c23a0e9081df302c82cc7643247df62
mm734 <mya30749@gmail.com>
2025-05-27 17:13:31 +0530

    yes commit

--- script.js
+++ script.js
@@ -118,3 +118,16 @@ function navigateTo(page) {
             console.log("Page not found");
     }
 }
+
+document.addEventListener('DOMContentLoaded', function()
+{
+    const menuIcon = document.getElementById('menuIcon');
+    const sidebar = document.getElementById('sidebar');
+    const closeBtn = document.getElementById('closeBtn')
+    menuIcon.addEventListener('click', function() {
+        sidebar.classList.add('active');
+    })
+    closeBtn.addEventListener('click', function() {
+        sidebar.classList.remove('active');
+    })
+});
